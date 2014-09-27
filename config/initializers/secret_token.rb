9a7d0e30f8e4b0d5b3e04a904d132201e82bd474
TiffanyAndrews <tiffanyjandrews@gmail.com>
2014-09-27 15:55:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnonymousDingo::Application.config.secret_key_base = '35430826de675d91ec5aafd7f3cd717ae72bfe9573fd972c1514d59ea40c71e619fc71007f0dabcbb49a0a70376198d841303b7ed0fa6837b29396a744c15f87'
