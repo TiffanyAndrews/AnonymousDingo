9c770f209cee5488038bc2476b048dde3a30f3b0
Theresa Luu <theresa.luu@gmail.com>
2014-09-27 20:17:22 -0700

    change goon table name to singular

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928020835) do
+ActiveRecord::Schema.define(version: 20140928031336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140928020835) do
     t.string   "name"
   end
 
-  create_table "goons", force: true do |t|
+  create_table "goon", force: true do |t|
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
