4e8cc3ed7b230542f7385b506f27313b512ca9e9
TiffanyAndrews <tiffanyjandrews@gmail.com>
2014-09-27 18:45:56 -0700

    add twilio to app

---
+++ app/utilities/sms.rb
@@ -0,0 +1,25 @@
+class SMS
+  ERROR = Twilio::REST::RequestError
+
+  attr_reader :phone_number
+
+  def initialize(phone_number)
+    @phone_number = phone_number
+  end
+
+  def deliver(new_message)
+    begin
+      client.account.messages.create(:body => new_message,
+                                     :to => phone_number,
+                                     :from => ENV['ATV_PHONE_NUMBER'])
+    rescue ERROR => e
+      Rails.logger.error "Failed to send the sms to #{phone_number}. " + e.message
+    end
+  end
+
+  private
+
+  def client
+    @client ||= Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+  end
+end
